8b2618098ea9778231ccbe12eb5abeaf4a262ddb
jb200957 <t2sudi00@students.oamk.fi>
2026-04-02 10:45:31 +0300

    feat: add save favourite endpoint

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import { ENV } from "./env.js";
-import * as schema from "../db7schema.js";
+import * as schema from "../db/schema.js";
 
 const sql = neon(ENV.DATABASE_URL);
-export const db = drixxle(sql, { schema });
+export const db = drizzle(sql, { schema });
