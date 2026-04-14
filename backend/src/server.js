82ae9ccd48ab342cc97b8d161e51ac1a489c01dc
jb200957 <t2sudi00@students.oamk.fi>
2026-04-14 19:30:12 +0300

    feat: add favourite endpoints (save & delete)

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,6 +12,7 @@ app.get("/api/health", (req, res) => {
 });
 
 
+
 app.post("/api/favourites", async (req, res) => {
     try {
         const { userId, recipeId, title, image, cookTime, servings } = req.body;
@@ -38,6 +39,40 @@ app.post("/api/favourites", async (req, res) => {
         res.status(500).json({ error: "Something went wrong" });
      }
 });
+
+app.get("/api/favourites/:userId", async (req, res) => {
+    try {
+        const { userId } = req.params;
+
+        const userFavourites = await db
+            .select()
+            .from(favouritesTable)
+            .where(eq(favouritesTable.userId, userId))
+        
+        res.json(userFavourites)
+
+    } catch (error) {
+        console.log("Error fetching the favourites", error);
+        res.status(500).json({ error: "Something went wrong" }); 
+    }
+})
+
+app.delete("/api/favourites/:userId/:recipeId", async (req, res) => {
+    try {
+        const { userId, recipeId } = req.params
+        
+        await db
+            .delete(favouritesTable)
+            .where(
+            and(eq(favouritesTable.userId, userId), eq(favouritesTable.recipeId, parseInt(recipeId)))
+        );
+
+        res.status(200).json({ message: "Favourite removed successfully" });
+    } catch (error) {
+       console.log("Error removing a favourite", error);
+       res.status(500).json({ error: "Something went wrong" }); 
+    }
+})
 app.listen(PORT , () => {
   console.log("Server is running123 on PORT:", PORT);
 })
